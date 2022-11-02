932fc2779cb563ada908874c1896dd3fedba33a9
Yasiru Lakshan <yasiru1999@gmail.com>
2022-11-02 11:55:29 +0530

    Create function for get count of element exist in array

--- timetable.py
+++ timetable.py
@@ -1,7 +1,25 @@
 import csv
 
+
 # inputFileName = input("Enter the name of the input file: ")
 # outputFileName = input("Enter the name of the output file: ")
+
+
+class Subject:
+    def __init__(self, id, varient, timeSlots, choosenTimeSlot, choosenRoom):
+        self.id = id
+        self.varient = varient
+        self.timeSlots = timeSlots
+        self.choosenTimeSlot = choosenTimeSlot
+        self.choosenRoom = choosenRoom
+
+
+def findCount(arr, element):
+    count = 0
+    for i in range(len(arr)):
+        if arr[i] == element:
+            count += 1
+    return count
 
 inputFileName = "input_file.csv"
 outputFileName = "output_file.csv"
@@ -16,4 +34,30 @@ with open(inputFileName, 'r') as inputFile:
         inpt.append(row)
 
 rooms = inpt.pop(-1)
+
+subjects = []
+
+for i in inpt:
+    subjects.append(Subject(i[0], i[1], i[2:], None, None))
+
+compalsory = []
+optional = []
+
+subject_finalized = 0
+
+while subject_finalized < len(inpt):
+    subject = subjects[subject_finalized]
+    timeGot = False
+    for time in subject.timeSlots:
+        if time not in compalsory:
+            if subject.varient == "c":
+                compalsory.append(time)
+                subject.choosenTimeSlot = time
+                timeGot = True
+            else:
+
+
+    if not timeGot:
+        subject.choosenTimeSlot = None
+        subject_finalized -= 1
 
