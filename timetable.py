bc84db91158c9f6f417572f0f88b60911cbc3220
Yasiru Lakshan <yasiru1999@gmail.com>
2022-11-02 10:20:49 +0530

    Input file read completed

--- timetable.py
+++ timetable.py
@@ -1,3 +1,15 @@
-inputFileName = input()
-outputFileName = input()
+import csv
 
+inputFileName = input("Enter the name of the input file: ")
+outputFileName = input("Enter the name of the output file: ")
+
+inpt = []
+
+#read input file
+with open(inputFileName, 'r') as inputFile:
+    csvReader = csv.reader(inputFile)
+
+    for row in csvReader:
+        inpt.append(row)
+
+print(inpt)
